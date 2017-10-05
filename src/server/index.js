642993695d98f3f0946121dde94d9c124dab9319
standley.lin <standleylin@gmail.com>
2017-10-05 15:55:48 +0800

    feat(server): hello world

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
 /* node , babel es6 */
-require("babel-core/register");
-require("babel-polyfill");
-require('./app.js');
+require('babel-core/register');
+require('babel-polyfill');
+require('./server-graphql');
